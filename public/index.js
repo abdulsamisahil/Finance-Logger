b9d27ba2989e3b1f267dca832dcecc549576e984
Abdul Sami <sami.kdr@live.se>
2021-12-09 14:11:31 +0100

    Finance logger finished

--- public/index.js
+++ public/index.js
@@ -1,13 +1,6 @@
 import { Invoice } from './classes/Invoice.js';
 import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/Payment.js';
-let docOne;
-let docTwo;
-docOne = new Invoice('Sami', 'web work', 500);
-docTwo = new Payment('Shoib', 'constructing work', 10000);
-let docs = [];
-docs.push(docOne);
-docs.push(docTwo);
 const form = document.querySelector('.new-item-form');
 //inputs 
 const type = document.querySelector('#type');
@@ -25,27 +18,4 @@ form.addEventListener('submit', (e) => {
         doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
     list.render(doc, type.value, 'end');
 });
-// Typescript generics
-const addUID = (obj) => {
-    let uid = Math.floor(Math.random() * 200);
-    return Object.assign(Object.assign({}, obj), { uid });
-};
-let obj = addUID({ name: 'Sami', age: 31 });
-console.log(obj);
-let obj2 = addUID({ name: 'Baqi', age: 33 });
-console.log(obj2);
-const docThree = {
-    uid: 45,
-    resourceName: 'sami',
-    data: ['apple', 'orange', 'pomogranate', 'banana']
-};
-const docFour = {
-    uid: 45,
-    resourceName: 'sami',
-    data: {
-        array1: ['apple', 'orange', 'pomogranate', 'banana'],
-        array2: ['juice', 'fresh', 'pomogranate', 'banana']
-    }
-};
-console.log(docFour);
 //Begin from #19
--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,6 @@ import { ListTemplate } from './classes/ListTemplate.js';
 import {Payment} from './classes/Payment.js'; 
 import {HasFormatter} from "./interfaces/HasFormatter.js"; 
 
-
-let docOne: HasFormatter; 
-let docTwo: HasFormatter; 
-
-docOne = new Invoice('Sami', 'web work', 500); 
-docTwo = new Payment('Shoib', 'constructing work', 10000); 
-
-let docs: HasFormatter[] = []; 
-
-docs.push(docOne); 
-docs.push(docTwo); 
-
 const form = document.querySelector('.new-item-form') as HTMLElement; 
 
 //inputs 
@@ -27,49 +15,15 @@ const amount = document.querySelector('#amount') as HTMLInputElement;
 const ul = document.querySelector('ul')!; 
 const list = new ListTemplate(ul); 
 
-form.addEventListener('submit', (e: Event) =>{
+form.addEventListener('submit', (e: Event) => {
     e.preventDefault(); 
     let doc: HasFormatter; 
 
     type.value === 'invoice' ? 
         doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber) : 
         doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+
     list.render(doc, type.value, 'end'); 
 }); 
 
-
-// Typescript generics
-
-const addUID = <T extends {name: string, age: number}>(obj: T) => {
-    let uid = Math.floor(Math.random() * 200); 
-    return {...obj, uid}; 
-}
-
-let obj = addUID({name: 'Sami', age: 31});
-console.log(obj); 
-let obj2 = addUID({name: 'Baqi', age: 33}); 
-console.log(obj2);
-
-
-interface Resource <T> {
-    uid: number, 
-    resourceName: string, 
-    data: T
-}
-
-const docThree : Resource <string[]> = {
-    uid: 45,
-    resourceName: 'sami', 
-    data: ['apple', 'orange', 'pomogranate', 'banana']
-}
-
-const docFour : Resource <object> = {
-    uid: 45,
-    resourceName: 'sami', 
-    data: {
-        array1:['apple', 'orange', 'pomogranate', 'banana'], 
-        array2:['juice', 'fresh', 'pomogranate', 'banana']
-    }
-}
-console.log(docFour); 
 //Begin from #19
